8433141a30e1fa341c0a4bd1b4f0b3e0bf6b6f59
iogod <johnson.roscoea@gmail.com>
2023-10-14 16:27:06 -0700

    w

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3gainlystorageeebcea69": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
