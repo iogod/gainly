a29374b8ca325f3a29c4464ff52edb5cc135014a
iogod <johnson.roscoea@gmail.com>
2023-10-14 21:45:35 -0700

    remove auth

--- src/App.js
+++ src/App.js
@@ -10,16 +10,18 @@ import {
 import styles from "@aws-amplify/ui-react/styles.css";
 
 
-function App() {
-  return (
-    <div className="App">
-      <div>
-        <Heading level={1}>Hello {"HERE"}</Heading>
-        <Button >Sign out</Button>
-        
-      </div>
+function App({ signOut, user }) { 
+  // ... 
+  // ...
+return (
+  <div style={styles.container}>
+    <Heading level={1}>Hello {user.username}</Heading>
+    <Button onClick={signOut}>Sign out</Button>
+    <h2>Amplify Todos</h2>
     </div>
-  );
+)
 }
+export default withAuthenticator(App);
 
-export default App;
+
+
